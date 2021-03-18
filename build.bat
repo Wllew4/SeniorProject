819eb9d2e48dee6f5ac7ffd4a6cb15395b70a4f4
Wllew4 <soupsepicgamesbusiness@gmail.com>
2021-03-18 13:39:33 -0400

    vastly improved batch file

--- build.bat
+++ build.bat
@@ -1,13 +1,25 @@
 @echo off
+setlocal enabledelayedexpansion
 
 set sourcefile=examples/helloworld.jan
 
-set sourcepaths=interpreter\src\*.cpp interpreter\src\util\*.cpp interpreter\src\parser\*.cpp
 set output=.\interpreter\bin\janelle.exe
 set includepaths=interpreter/src
 
-::  Compile
-g++ %sourcepaths% -o %output% -I %includepaths%
+::  Build intermediates
+set /A i=0
+mkdir intermediates
+for /R "interpreter\src\" %%f in ("*.cpp") DO (
+    set /A i+=1
+    g++ -c %%f -I %includepaths% -o intermediates\!i!.o
+)
+
+::  Compile executable
+g++ intermediates\*.o -o %output%
+
+::  Clean up intermediates
+del /Q intermediates
+rmdir /Q intermediates
 
 ::  Execute
 %output% %sourcefile%
