440686473acaa402e0dbc33e4d9919c20cfb8681
Wllew4 <soupsepicgamesbusiness@gmail.com>
2021-03-22 02:51:18 +0000

    Cleaned up bash

--- build.sh
+++ build.sh
@@ -3,23 +3,7 @@ flags=--debug-printtokens
 
 output=interpreter/bin/janelle.exe
 includepaths=interpreter/src
+sources=$(find ./interpreter/src -type f -iname *.cpp)
 
-#		Build intermediates
-i=0
-mkdir intermediates
-for f in interpreter/src/*.cpp interpreter/src/**/*.cpp; do
-		echo $f
-    i=$[i+1]
-    g++ -c $f -I $includepaths -o intermediates/$i.o
-done
-
-#		Compile executable
-[ ! -d "intermediates/bin" ]; mkdir interpreter/bin
-g++ intermediates/*.o -o $output
-
-#		Clean up intermediates
-rm -r intermediates
-
-#		Execute
-#	segmentation fault?
+g++ -o $output $sources -I $includepaths
 $output $sourcefile $flags
